815ba51485635568d7116742118d5e4dd5a955bb
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-22 19:18:56 +0530

    UC3 :- Junit Test Case for Check Email is Write in correct way

--- test/java/RegistrationTest.java
+++ test/java/RegistrationTest.java
@@ -4,31 +4,51 @@ import org.junit.jupiter.api.Test;
 public class RegistrationTest
 {
     @Test
-    public void TestFirstNameValidThenTrue()
+    public void TestEmailFirstNameIsLowerCaseThenTrue()
     {
         UserRegistration checkname= new UserRegistration();
-        boolean Result = checkname.lastNameValid("More");
+        boolean Result = checkname.emailValid("abc.xyz@bridgelabz.com.in");
         Assertions.assertEquals(true, Result);
     }
     @Test
-    public void TestFirstNameValidThenfalse()
+    public void TestEmailFirstNameIsLowerCaseThenfalse()
     {
         UserRegistration checkname= new UserRegistration();
-        boolean Result = checkname.lastNameValid("more");
+        boolean Result = checkname.emailValid("Abc.xyz@bridgelabz.com.in");
         Assertions.assertEquals(false, Result);
     }
     @Test
-    public void TestFirstNameThreeMinimumTrue()
+    public void TestEmailLastShouldBe_Com_And_In_ThenTrue()
     {
         UserRegistration checkname= new UserRegistration();
-        boolean Result = checkname.lastNameValid("More");
+        boolean Result = checkname.emailValid("abc.xyz@bridgelabz.com");
+        boolean Result1 = checkname.emailValid("abc.xyz@bridgelabz.com.in");
+        Assertions.assertEquals(true, Result);
+        Assertions.assertEquals(true, Result1);
+    }
+
+    @Test
+    public void TestEmailLastShouldBe_Com_And_In_Thenfalse() {
+        UserRegistration checkname = new UserRegistration();
+        boolean Result = checkname.emailValid("asbc@huj");
+        Assertions.assertEquals(false, Result);
+
+    }
+    @Test
+    public void TestEmailSignAtTheRateIsCompulsaryThenTrue()
+    {
+        UserRegistration checkname = new UserRegistration();
+        boolean Result = checkname.emailValid("abc.xyz@bridgelabz.com.in");
         Assertions.assertEquals(true, Result);
     }
+
     @Test
-    public void TestFirstNameThreeMinimumFalse()
+    public void TestEmailSignAtTheRateIsCompulsaryThenFalse()
     {
-        UserRegistration checkname= new UserRegistration();
-        boolean Result = checkname.lastNameValid("Mo");
+        UserRegistration checkname = new UserRegistration();
+        boolean Result = checkname.emailValid("abc.xyzgmail.com");
         Assertions.assertEquals(false, Result);
+
     }
 }
+
