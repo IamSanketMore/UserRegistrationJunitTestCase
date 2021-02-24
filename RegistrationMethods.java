3c17fb4eadf5ceca3a8aeb32c30e09b499542865
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-24 20:13:20 +0530

    UC11- User Registration Methods

--- RegistrationMethods.java
+++ RegistrationMethods.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class RegistrationMethods
 {
-    public String checkFirstName(String firstName)
+    public boolean checkFirstName(String firstName)
     {
         //------------Regex------------------
         String regex = "[A-Z]{1}[a-z]{2,}";
@@ -14,16 +14,16 @@ public class RegistrationMethods
         //-------------Check valid or Not-----
         if(matchChecker.matches()) {
             System.out.println("Valid First Name");
-            return "HAPPY";
+            return true;
         }
         else
         {
             System.out.println("InValid First Name");
-            return "SAD";
+            return false;
         }
     }
 
-    public String checkLastName(String lastName) {
+    public boolean checkLastName(String lastName) {
         //------------Regex------------------
         String regex = "[A-Z]{1}[a-z]{2,}";
 
@@ -34,16 +34,16 @@ public class RegistrationMethods
         if (matchChecker.matches())
         {
             System.out.println("Valid Last Name");
-            return "HAPPY";
+            return true;
         }
         else
         {
             System.out.println("InValid Last Name");
-            return "SAD";
+            return false;
         }
     }
 
-    public String checkEmail(String emailId)
+    public boolean checkEmail(String emailId)
     {
         //------------Regex------------------
         String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]+)?(@[a-z0-9]+)([.][a-z]{2,4})([.][a-z]{2})?$";
@@ -54,16 +54,16 @@ public class RegistrationMethods
         if(matchChecker.matches())
         {
             System.out.println("Valid Email");
-            return "HAPPY";
+            return true;
         }
         else
         {
             System.out.println("InValid Email");
-            return "SAD";
+            return false;
         }
     }
 
-    public String checkMobile(String mobileNumber)
+    public boolean checkMobile(String mobileNumber)
     {
         //------------Regex------------------
         String regex = "^[0-9]{2}[ ][0-9]{10}$";
@@ -73,15 +73,15 @@ public class RegistrationMethods
         //-------------Check valid or Not-----
         if(matchChecker.matches()) {
             System.out.println("Valid Mobile Number");
-            return "HAPPY";
+            return true;
         }
         else {
             System.out.println("InValid Mobile Number");
-            return "SAD";
+            return false;
         }
     }
 
-    public String checkPasswordRule(String passWord) {
+    public boolean checkPasswordRule(String passWord) {
         //------------Regex------------------
         String regex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:;<>?/~_+-=|\\]]).{8,}$";
         Pattern patternChecker = Pattern.compile(regex);
@@ -90,11 +90,27 @@ public class RegistrationMethods
         //-------------Check valid or Not-----
         if (matchChecker.matches()) {
             System.out.println("Valid Password");
-            return "HAPPY";
+            return true;
         }
         else {
             System.out.println("InValid Password");
-            return "SAD";
+            return false;
+        }
+    }
+
+    public boolean checkEmailSamples(String email)
+    {
+        String regex = "^abc[a-zA-Z0-9.+-]*@[a-z0-9]*[.][a-z]{2,5}[.,a-z]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches())
+        {
+            System.out.println("Entered Email Sample is Valid");
+            return true;
+        } else
+        {
+            System.out.println("Entered Email Sample is Invalid");
+            return false;
         }
     }
 }
