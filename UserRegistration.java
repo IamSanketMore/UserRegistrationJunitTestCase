ce3a3f75f8dd17ae4a34fc04ee1bee288b13f2f4
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-24 20:12:30 +0530

    UC11- User Registration Main Class

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,38 +3,49 @@ import java.util.*;
 public class UserRegistration
 {
 
-        public static void main(String args [])
-        {
-            Scanner scanReader = new Scanner(System.in);
+    public static void main(String args [])
+    {
+        Scanner scanReader = new Scanner(System.in);
 
-            RegistrationMethods  checkObj = new RegistrationMethods ();
+        RegistrationMethods  checkObj = new RegistrationMethods ();
 
-            //----------Enter First Name-------------------//
-            System.out.println("Enter First Name:-");
-            String FirstName = scanReader.nextLine();
-            checkObj.checkFirstName(FirstName);
+        //----------Enter First Name-------------------//
+        System.out.println("Enter First Name:-");
+        String FirstName = scanReader.nextLine();
+        checkObj.checkFirstName(FirstName);
 
-            //----------Enter Last Name--------------------//
-            System.out.println("Enter Last Name:-");
-            String LastName = scanReader.nextLine();
-            checkObj.checkLastName(LastName);
+        //----------Enter Last Name--------------------//
+        System.out.println("Enter Last Name:-");
+        String LastName = scanReader.nextLine();
+        checkObj.checkLastName(LastName);
 
-            //----------Enter Email------------------------//
-            System.out.println("Enter Email:-");
-            String email = scanReader.nextLine();
-            checkObj.checkEmail(email);
+        //----------Enter Email------------------------//
+        System.out.println("Enter Email:-");
+        String email = scanReader.nextLine();
+        checkObj.checkEmail(email);
 
-            //----------Enter Mobile NUmber----------------//
-            System.out.println("Enter Mobile Number:-");
-            String mobileNumber = scanReader.nextLine();
-            checkObj.checkMobile(mobileNumber);
+        //----------Enter Mobile NUmber----------------//
+        System.out.println("Enter Mobile Number:-");
+        String mobileNumber = scanReader.nextLine();
+        checkObj.checkMobile(mobileNumber);
 
-            //----------Enter Password--------------------//
-            System.out.println("Enter Password :-");
-            String passWord = scanReader.nextLine();
-            checkObj.checkPasswordRule(passWord);
+        //----------Enter Password--------------------//
+        System.out.println("Enter Password :-");
+        String passWord = scanReader.nextLine();
+        checkObj.checkPasswordRule(passWord);
 
-            //----------Scanner Close---------------------//
-            scanReader.close();
-        }
+        System.out.println("Check Following Email Samples is valid or Invalid");
+        checkObj.checkEmailSamples("abc@yahoo.com");
+        checkObj.checkEmailSamples("abc-100@yahoo.com");
+        checkObj.checkEmailSamples("abc.100@yahoo.com");
+        checkObj.checkEmailSamples("abc111@abc.com");
+        checkObj.checkEmailSamples("abc-100@abc.net");
+        checkObj.checkEmailSamples("abc.100@abc.com.au");
+        checkObj.checkEmailSamples("abc@1.com");
+        checkObj.checkEmailSamples("abc@gmail.com.com");
+        checkObj.checkEmailSamples("abc+100@gamil.com");
+
+        //----------Scanner Close---------------------//
+        scanReader.close();
+    }
 }
