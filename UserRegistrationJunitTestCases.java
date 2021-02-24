26a1048bc965d14c0dd1654c5a6f4ecc24fc5d7c
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-24 19:27:16 +0530

    UC10-User REgistration Methods With Mood analyser Concepts And Junit Test Cases for That

--- UserRegistrationJunitTestCases.java
+++ UserRegistrationJunitTestCases.java
@@ -1,47 +1,78 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UserRegistrationJunitTestCases 
+public class UserRegistrationJunitTestCases
 {
-	    @Test
-	    public void TestFirstNameValidThenTrue()
-	    {
-	    	RegistrationMethods checkObj = new RegistrationMethods();
-	        boolean Result = checkObj.checkFirstName("Sanket");
-	        Assert.assertEquals(true, Result);
-	    }
-	    
-	    @Test
-	    public void TestLastNameValidThenTrue()
-	    {
-	    	RegistrationMethods checkObj= new RegistrationMethods();
-	        boolean Result = checkObj.checkLastName("More");
-	        Assert.assertEquals(true, Result);
-	    }
-	    
-	    @Test
-	    public void TestEmailNameValidThenTrue()
-	    {
-	    	RegistrationMethods checkObj = new RegistrationMethods();
-	        boolean Result = checkObj.checkEmail("abc.xyz@bridgelabz.com.in");
-	        Assert.assertEquals(true, Result);
-	    }
-	    
-	    @Test
-	    public void TestMobileValidThenTrue()
-	    {
-	    	RegistrationMethods checkObj= new RegistrationMethods();
-	        boolean Result = checkObj.checkMobile("91 1234567890");
-	        Assert.assertEquals(true, Result);
-	    }
-	    
-	    @Test
-	    public void TestPasswordValidThenTrue()
-	    {
-	    	RegistrationMethods checkObj= new RegistrationMethods();
-	        boolean Result = checkObj.checkPasswordRule("Sanket@123");
-	        Assert.assertEquals(true, Result);
-	    }
-	    
-	    
+    @Test
+    public void TestFirstNameValidThenHAPPY()
+    {
+        RegistrationMethods checkObj = new RegistrationMethods();
+        String Result = checkObj.checkFirstName("Sanket");
+        Assert.assertEquals("HAPPY", Result);
+    }
+    @Test
+    public void TestFirstNameValidThenSAD()
+    {
+        RegistrationMethods checkObj = new RegistrationMethods();
+        String Result = checkObj.checkFirstName("sanket");
+        Assert.assertEquals("SAD", Result);
+    }
+    @Test
+    public void TestLastNameValidThenHAPPY()
+    {
+        RegistrationMethods checkObj= new RegistrationMethods();
+        String Result = checkObj.checkLastName("More");
+        Assert.assertEquals("HAPPY", Result);
+    }
+    @Test
+    public void TestLastNameValidThenSAD()
+    {
+        RegistrationMethods checkObj = new RegistrationMethods();
+        String Result = checkObj.checkLastName("sanket");
+        Assert.assertEquals("SAD", Result);
+    }
+
+    @Test
+    public void TestEmailNameValidThenHAPPY()
+    {
+        RegistrationMethods checkObj = new RegistrationMethods();
+        String Result = checkObj.checkEmail("abc.xyz@bridgelabz.com.in");
+        Assert.assertEquals("HAPPY", Result);
+    }
+    @Test
+    public void TestEmailNameValidThenSAD()
+    {
+        RegistrationMethods checkObj = new RegistrationMethods();
+        String Result = checkObj.checkEmail("abc.xyzbridgelabz.c");
+        Assert.assertEquals("SAD", Result);
+    }
+
+    @Test
+    public void TestMobileValidThenHAPPY()
+    {
+        RegistrationMethods checkObj= new RegistrationMethods();
+        String Result = checkObj.checkMobile("91 1234567890");
+        Assert.assertEquals("HAPPY", Result);
+    }
+    @Test
+    public void TestMobileValidThenSAD()
+    {
+        RegistrationMethods checkObj= new RegistrationMethods();
+        String Result = checkObj.checkMobile("9 1234567890");
+        Assert.assertEquals("SAD", Result);
+    }
+    @Test
+    public void TestPasswordValidThenHAPPY()
+    {
+        RegistrationMethods checkObj= new RegistrationMethods();
+        String Result = checkObj.checkPasswordRule("Sanket@123");
+        Assert.assertEquals("HAPPY", Result);
+    }
+    @Test
+    public void TestPasswordValidThenSAD()
+    {
+        RegistrationMethods checkObj= new RegistrationMethods();
+        String Result = checkObj.checkPasswordRule("sanket@123");
+        Assert.assertEquals("SAD", Result);
+    }
 }
