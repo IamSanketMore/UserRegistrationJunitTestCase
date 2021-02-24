4aef93be32e92fe8145c48181651118801073e4b
IamSanketMore <sanketmore.999.m@gmail.com>
2021-02-24 20:38:05 +0530

    UC12- Custom User-REgistration-Exception Created

---
+++ UserRegistrationException.java
@@ -0,0 +1,14 @@
+public class UserRegistrationException extends Exception
+{
+    enum ExceptionType
+    {
+        NullEnter,EmptyEnter
+    }
+    ExceptionType type;
+
+    public UserRegistrationException(String message,ExceptionType type)
+    {
+        super(message);
+        this.type= type;
+    }
+}
